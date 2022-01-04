f3de138272570b6b70589bea5373d4d8786f2ab0
Md Mehedi Hasan <45948603+mehedi-01@users.noreply.github.com>
2022-01-04 21:43:33 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
-import { fetchData} from './api';
+import {fetchData} from './api';
 
 class App extends React.Component{
     state = {
-        data : {}
+        data : {},
     }
 
     async componentDidMount(){
-        const data = await fetchData();
+        const fetchedData = await fetchData();
 
-        this.setState({data: fetchData});
+        this.setState({data: fetchedData});
     }
     render(){
         const {data} = this.state;
+
         return (
             <div className={styles.container}>
                 <Cards  data={data}/>
                 <Chart />
                 <CountryPicker />
-            </div>
+            </div> 
         )
     }
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const url ='https://covid19.mathdro.id/api';
+const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () =>{
+export const fetchData = async () => {
     try {
-        const {data:{confirmed, recovered, death, lastUpdate}} =  await axios.get(url);
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
 
-       return {confirmed,recovered,death,lastUpdate};
+        return { confirmed, recovered, deaths, lastUpdate };
     } catch (error) {
-        
+
     }
 }
